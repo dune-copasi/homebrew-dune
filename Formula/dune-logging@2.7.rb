1acaee0e567d2d8718921ccaa04b75940993bd5e
Santiago Ospina De Los Rios <sospinar@gmail.com>
2021-01-30 23:07:33 +0100

    Fix url and shasum of dunelogging

--- Formula/dune-logging@2.7.rb
+++ Formula/dune-logging@2.7.rb
@@ -1,8 +1,8 @@
 class DuneLoggingAT27 < Formula
   desc ""
   homepage "https://gitlab.dune-project.org"
-  url "https://gitlab.dune-project.org/core/dune-logging/-/archive/releases/2.7/dune-logging-releases-2.7.tar.gz"
-  # sha256 "e3c43537ea4857699b860d3a39ef8ef0b3669f2f6755834fce775b11dacc81ad"
+  url "https://gitlab.dune-project.org/staging/dune-logging/-/archive/releases/2.7/dune-logging-releases-2.7.tar.gz"
+  sha256 "32b5d4b559d54d6ed6b8a1ebc8e0cff1c4113e03d5d129426b0c4c45e6f85da2"
   license "GPL-2.0"
 
   depends_on "dune-copasi/dune/dune-common"
