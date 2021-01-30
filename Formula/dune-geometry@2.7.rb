75db5a4f3867feb6952e7c4a4f2d8b81e258b39e
Santiago Ospina De Los Rios <sospinar@gmail.com>
2021-01-30 22:59:55 +0100

    adding geometry and logging

---
+++ Formula/dune-geometry@2.7.rb
@@ -0,0 +1,18 @@
+class DuneGeometryAT27 < Formula
+  desc ""
+  homepage "https://gitlab.dune-project.org"
+  url "https://gitlab.dune-project.org/core/dune-geometry/-/archive/releases/2.7/dune-geometry-releases-2.7.tar.gz"
+  sha256 "e3c43537ea4857699b860d3a39ef8ef0b3669f2f6755834fce775b11dacc81ad"
+  license "GPL-2.0"
+
+  depends_on "dune-copasi/dune/dune-common"
+
+  def install
+    system "cmake", "-DBUILD_SHARED_LIBS=1", ".", *std_cmake_args
+    system "make", "install"
+  end
+
+  test do
+    system "false"
+  end
+end
