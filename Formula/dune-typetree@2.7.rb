66d78bdb9455506e8a240c089ef124055f0495ea
Santiago Ospina De Los Rios <sospinar@gmail.com>
2021-01-31 04:08:45 +0100

    Use dune-copasi specific version

--- Formula/dune-typetree@2.7.rb
+++ Formula/dune-typetree@2.7.rb
@@ -1,8 +1,8 @@
 class DuneTypetreeAT27 < Formula
   desc "Packaging for dune-typetree: a toolbox for solving PDEs -- typed tree template library"
   homepage "https://gitlab.dune-project.org"
-  url "https://gitlab.dune-project.org/staging/dune-typetree/-/archive/v2.7.1/dune-typetree-v2.7.1.tar.gz"
-  sha256 "74e392117fbcc7e001b4f1bfdf68d06827ce6cfa84f1e3eaf17faca8ad8c610e"
+  url "https://gitlab.dune-project.org/copasi/dune-typetree/-/archive/dune-copasi-v0.3/dune-typetree-dune-copasi-v0.3.tar.gz"
+  sha256 "e4be8734093506bf5c81bf227bfb4f5a7cc2286cbc730ec024f6a088032fc771"
   # license any_of: [
   #   "GPL-2.0-only" => { with: "DUNE-exception" },
   #   "LGPL-3.0-or-later"
