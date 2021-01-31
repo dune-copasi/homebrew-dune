23035ab6e64b46e7c7d72a0a6e94d91ac15df740
Santiago Ospina De Los Rios <sospinar@gmail.com>
2021-01-31 04:34:38 +0100

    Add multidomaingrid and rules for pdelab

---
+++ Formula/dune-multidomaingrid@2.7.rb
@@ -0,0 +1,29 @@
+class DuneMultidomaingridAT27 < Formula
+  option "without-dune-copasi-support", "Leave out support for dune-copasi"
+  desc "Packaging for dune-typetree: a toolbox for solving PDEs -- typed tree template library"
+  homepage "https://gitlab.dune-project.org"
+
+  if build.without? "dune-copasi-support"
+    url "https://gitlab.dune-project.org/extensions/dune-multidomaingrid/-/archive/v2.7.0/dune-multidomaingrid-v2.7.0.tar.gz"
+    sha256 "0898f43737f70d090ae95db91303bb8112534974c178d5a593c4d7e154f0ad16"
+  else
+    url "https://gitlab.dune-project.org/copasi/dune-multidomaingrid/-/archive/dune-copasi-v0.3/dune-multidomaingrid-dune-copasi-v0.3.tar.gz"
+    sha256 "6ef29d1090ed3865879c21fb8bb71eec076f5495c30afa46746a15589944a3e4"
+  end
+  # license any_of: [
+  #   "GPL-2.0-only" => { with: "DUNE-exception" },
+  #   "LGPL-3.0-or-later"
+  # ]
+
+  depends_on "dune-common@2.7"
+  depends_on "dune-copasi/tap/dune-typetree@2.7"
+
+  def install
+    system "cmake", "-DBUILD_SHARED_LIBS=1", ".", *std_cmake_args
+    system "make", "install"
+  end
+
+  test do
+    system "false"
+  end
+end
--- Formula/dune-pdelab@2.7.rb
+++ Formula/dune-pdelab@2.7.rb
@@ -1,8 +1,16 @@
 class DunePdelabAT27 < Formula
   desc "Packaging for dune-pdelab: a toolbox for solving PDEs -- discretization module"
   homepage "https://gitlab.dune-project.org"
-  url "https://gitlab.dune-project.org/pdelab/dune-pdelab/-/archive/releases/2.7/dune-pdelab-releases-2.7.tar.gz"
-  sha256 "e0d82fa43c744bcab0f486e69f421553f3329771cd1162bd738831e148ead1e2"
+
+  if build.without? "dune-copasi-support"
+    url "https://gitlab.dune-project.org/pdelab/dune-pdelab/-/archive/releases/2.7/dune-pdelab-releases-2.7.tar.gz"
+    sha256 "e0d82fa43c744bcab0f486e69f421553f3329771cd1162bd738831e148ead1e2"
+    depends_on "dune-copasi/tap/dune-typetree@2.7" => "without-dune-copasi-support"
+  else
+    url "https://gitlab.dune-project.org/copasi/dune-pdelab/-/archive/dune-copasi-v0.3/dune-pdelab-dune-copasi-v0.3.tar.gz"
+    sha256 "27e842896eb916c2725388014260676f1188cb2c041a6eb79e54965b1dfc0e29"
+    depends_on "dune-copasi/tap/dune-typetree@2.7"
+  end
   # license any_of: [
   #   "GPL-2.0-only" => { with: "DUNE-exception" },
   #   "LGPL-3.0-or-later"
@@ -11,7 +19,6 @@ class DunePdelabAT27 < Formula
   depends_on "dune-common@2.7"
   depends_on "dune-grid@2.7"
   depends_on "dune-istl@2.7"
-  depends_on "dune-typetree@2.7"
   depends_on "dune-localfunctions@2.7"
   depends_on "dune-functions@2.7"
   depends_on "eigen"
