78cb11ff2357f37542d042d351f2c96efde6c729
Santiago Ospina De Los Rios <sospinar@gmail.com>
2021-01-31 03:57:43 +0100

    Fix some license syntaxes

--- Formula/dune-logging@2.7.rb
+++ Formula/dune-logging@2.7.rb
@@ -3,11 +3,7 @@ class DuneLoggingAT27 < Formula
   homepage "https://gitlab.dune-project.org"
   url "https://gitlab.dune-project.org/staging/dune-logging/-/archive/releases/2.7/dune-logging-releases-2.7.tar.gz"
   sha256 "32b5d4b559d54d6ed6b8a1ebc8e0cff1c4113e03d5d129426b0c4c45e6f85da2"
-  license any_of: [
-    "GPL-2.0-only" => { with: "DUNE-exception" },
-    "LGPL-3.0-or-later",
-    "BSD-2-Clause"
-  ]
+  license any_of: [ "GPL-2.0-only" => { with: "DUNE-exception" }, "LGPL-3.0-or-later", "BSD-2-Clause" ]
 
   depends_on "dune-common@2.7"
   depends_on "fmt"
