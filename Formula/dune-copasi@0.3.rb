90a3948896bdb2cb94969046db03ce0f6f5f1045
Santiago Ospina De Los Rios <sospinar@gmail.com>
2021-01-31 21:32:29 +0100

    add typetree dependency

--- Formula/dune-copasi@0.3.rb
+++ Formula/dune-copasi@0.3.rb
@@ -5,6 +5,7 @@ class DuneCopasiAT03 < Formula
   sha256 "18b5bf3562d148486cfd922fecd92a64e5412589f2d96fa86e1792352f9d8f98"
   license "BSD-2-clause"
 
+  depends_on "dune-typetree@2.7" => "with-support-dune-copasi-v0.3"
   depends_on "dune-pdelab@2.7" => "with-support-dune-copasi-v0.3"
   depends_on "dune-logging@2.7" => "with-support-dune-copasi-v0.3"
   depends_on "dune-multidomaingrid@2.7" => "with-support-dune-copasi-v0.3"
