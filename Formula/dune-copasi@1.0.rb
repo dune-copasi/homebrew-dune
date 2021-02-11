75a42465cab9cce87a817fb58693fef257f0af19
Santiago Ospina De Los Ríos <sospinar@gmail.com>
2021-02-11 03:50:20 +0100

    Add option to install development tools

--- Formula/dune-copasi@1.0.rb
+++ Formula/dune-copasi@1.0.rb
@@ -14,24 +14,42 @@ class DuneCopasiAT10 < Formula
 
   license "BSD-2-clause"
 
+  option "with-devel", "Install development tools"
+
   depends_on "muparser"
   depends_on "libtiff"
   depends_on "open-mpi"
-  depends_on "cmake" => :build
-  depends_on "gcc" => :build
-  depends_on "dune-logging@2.7" => :build
-  depends_on "dune-copasi/tap/dune-copasi-multidomaingrid@1.0" => :build
-  depends_on "dune-copasi/tap/dune-copasi-pdelab@1.0" => :build
+
+  if build.with? "devel"
+    depends_on "cmake"
+    depends_on "gcc"
+    depends_on "dune-logging@2.7"
+    depends_on "dune-copasi/tap/dune-copasi-multidomaingrid@1.0"
+    depends_on "dune-copasi/tap/dune-copasi-pdelab@1.0"
+  else
+    depends_on "cmake" => :build
+    depends_on "gcc" => :build
+    depends_on "dune-logging@2.7" => :build
+    depends_on "dune-copasi/tap/dune-copasi-multidomaingrid@1.0" => :build
+    depends_on "dune-copasi/tap/dune-copasi-pdelab@1.0" => :build
+  end
 
   def install
     ENV.deparallelize
     system "cmake",
-    *std_cmake_args,
-    *DuneCommonAT27.cmake_args,
-    "-DDUNE_COPASI_SD_EXECUTABLE=ON",
-    "-DDUNE_USE_FALLBACK_FILESYSTEM=ON",
-    "."
-    system "make", "install"
+      *std_cmake_args,
+      *DuneCommonAT27.cmake_args,
+      "-DDUNE_COPASI_SD_EXECUTABLE=ON",
+      "-DDUNE_USE_FALLBACK_FILESYSTEM=ON",
+      "."
+
+    install_args = %W[
+      --component Libraries
+      --component Runtime
+    ]
+    install_args << "--component Development" if build.with? "devel"
+    install_args << "--component Unspecified" if build.with? "devel"
+    system "cmake", "--install", ".", *install_args
   end
 
   test do
